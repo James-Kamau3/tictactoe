11456ff2b8557221c1c007289d2deb4d9096a48f
James <kamaujames3705@gmail.com>
2023-07-25 11:22:31 +0300

    Tictactoe

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Board from './Components/Board';
+
+
+function App() {
+  return(
+    <div className='app'>
+  <Board />
+  </div>
+  )
+  
+}
+export default App
+
